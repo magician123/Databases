35d337f11cb00f817e87e1cc44861e7e91e84247
Tshepiso <timarape.gmail.com>
2020-11-19 14:03:15 +0200

    Added simple quesries

---
+++ PostgreSQL/CreateStudents.sql
@@ -0,0 +1,17 @@
+create table students
+(
+	student_no integer,
+	student_name varchar(20),
+	age integer
+);
+
+insert into students values (1, 'Michael', 19);
+insert into students values (2, 'Doug', 18);
+insert into students values (3, 'Samantha', 21);
+insert into students values (4, 'Pete', 20);
+insert into students values (5, 'Ralph', 19);
+insert into students values (6, 'Arnold', 22);
+insert into students values (7, 'Michael', 19);
+insert into students values (8, 'Jack', 19);
+insert into students values (9, 'Rand', 17);
+insert into students values (10, 'Sylvia', 20);
